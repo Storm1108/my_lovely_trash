f6cfd907535563340f6e306c0d6c38a24995f4a5
Storm1108 <mail.laser@yandex.ru>
2022-08-07 15:55:45 +0300

    Командный проект Урок 2

---
+++ Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿ //  14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+ //14 -> нет 
+ //46 -> нет 
+//161 -> да
+
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost_1 = a % 7;
+int ost_2 = a % 23;
+
+if (ost_1 == 0 && ost_2 == 0) {
+    System.Console.WriteLine("Да");
+}
+else System.Console.WriteLine("Нет");
