3aac6bceb2c98beaa5abd58583788cedfcbc6a9f
Storm1108 <mail.laser@yandex.ru>
2022-08-20 12:13:46 +0300

    Перенес из общей папка задания в папку для ДЗ

---
+++ Homework1/Hw3/31/Task21(normal)/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 12321;
+int nchar(int number, int pos){
+     string copy = number.ToString();
+     int N = copy.Length;
+     return Convert.ToInt32(copy[N - pos]);
+}
+if (nchar(n, 1) * 10 + nchar(n, 2) == nchar(n, 5) * 10 + nchar(n, 4)) 
+System.Console.WriteLine("Number " + n + " is palindrom");
+else
+System.Console.WriteLine("Number " + n + " is not a palindrom");
