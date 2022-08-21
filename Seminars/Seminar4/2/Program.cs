9007394c9ddb6db8d86850caadcd4e12653e88eb
Storm1108 <mail.laser@yandex.ru>
2022-08-21 16:33:16 +0300

    Порешал на семинаре

---
+++ Seminars/Seminar4/2/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = 3;
+int n = 4;
+double[,] A = new double[m, n];
+for (int i = 0; i < A.GetLength(0); i++)
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        A[i, j] = (new Random().NextDouble()) * new Random().Next(-100, 100);
+        System.Console.Write($"{A[i, j]}    ");
+    }
+    System.Console.WriteLine();
+}
---
+++ Seminars/Seminar4/3/Program.cs
@@ -0,0 +1,35 @@
+﻿int n = 3;
+int k = 4;
+int[,] A = new int[n, k];
+
+void Fill(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            A[i, j] = new Random().Next(-100, 100);
+            System.Console.Write($"{A[i, j]}    ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void Change(int[,] A)
+{
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < A.GetLength(1); j++)
+        {
+            if (A[i, j] % 2 == 0)
+            {
+                A[i, j] = -A[i, j];
+            }
+            System.Console.Write($"{A[i, j]}    ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Fill(A);
+System.Console.WriteLine();
+Change(A);
