1baf4f8e14ae367a43b6007472568fb0fe18764e
Storm1108 <mail.laser@yandex.ru>
2022-08-28 15:45:02 +0300

    Игры с коллекциями

--- Additional_tasks/Task45/Program.cs
+++ Additional_tasks/Task45/Program.cs
@@ -1,2 +1,18 @@
 ﻿// 45. Показать числа Фибоначчи
-int[]
+int[] fibbonachiFill(){
+    int[] fibCurrent = new int[200];
+    fibCurrent[0] = 1;
+    fibCurrent[1] = 1;
+    for (int count = 2; fibCurrent[count] > 0; count++)
+    {
+        {
+            fibCurrent[count] = fibCurrent[count-1] + fibCurrent[count-2];
+        }
+    }
+    return fibCurrent;
+}
+void arrayPrint(int[] array){
+    System.Console.Write($"Массив: ( {string.Join(", ", array)})");
+}
+arrayPrint(fibbonachiFill());
+List<int> fibbonachi = new List<int>() {1, 1};
